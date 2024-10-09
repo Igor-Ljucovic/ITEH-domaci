c29390d5b65c0a4ece2fb13ae4a1414da9421f8e
Igor <igorljucovic99@gmail>
2024-10-09 18:45:02 +0200

    Initial commit with all project files

--- index-kostur.php
+++ index-kostur.php
@@ -11,12 +11,12 @@
         <div class="main-div">
             <form method="POST" action="#">
                 <div class="container">
-                    <label class="username">Korisnicko ime</label>
+                    <label class="username">username</label>
                     <input type="text" name="username" class="form-control"  required>
                     <br>
-                    <label for="password">Lozinka</label>
+                    <label for="password">password</label>
                     <input type="password" name="password" class="form-control" required>
-                    <button type="submit" class="btn btn-primary" name="submit">Prijavi se</button>
+                    <button type="submit" class="btn btn-primary" name="submit">log in</button>
                 </div>
 
             </form>
